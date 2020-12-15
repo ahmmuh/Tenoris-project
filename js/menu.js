afdedc281cfd4b5814dc4765b1617d49a1e2a047
Ahmed Mukhtar Muhammed <ahmmuh@MacBook-Air-som-tillhor-Ahmed.local>
2020-12-15 09:34:57 +0100

    updated some files

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,14 @@
 "use strict";
+
+// toggle menu
+const toggleMenu = document.querySelector(".toggle-icon");
+
+const toggleHandler = () => {
+  const menuNotis = "Hej! Har inte hunnit skriva kod för det";
+  alert(menuNotis);
+};
+toggleMenu.addEventListener("click", toggleHandler);
+
 // Change menu BGC on scroll
 
 window.onscroll = () => {
